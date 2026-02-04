ca4e67f29a82c1fc688a630ea239bb57330cf2b8
Achassqt <sachsqt@gmail.com>
2026-02-04 22:44:07 +0100

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script src="https://unpkg.com/@phosphor-icons/web" async></script>
       </head>
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
